f65ce9806d17d38ad9eee0a0f01ab1d837745789
Alan Kruppa <alankruppa@gmail.com>
2015-05-25 22:55:52 -0400

    some punctuation corrections to the master list
    -removed pre-comma spaces
    -removed periods and exclamation points

--- challenge_data_model.py
+++ challenge_data_model.py
@@ -1,14 +1,14 @@
 # -----------------------------------------------------------------------------
 # description
 #
-#  a very basic working version of commands to generate a tokenized word
+# a very basic working version of commands to generate a tokenized word
 # list and associated nltk text class for further nlp resulting in a 
 # sorted list of the most frequently occurring phrases found in the cs 
 # rep's messages
 #
 # tokenize list and then concatenate it by length, in order to take
 # advantage of the nltk FreqDist function for sorting occurrences of
-# phrases in addition to single words. the underscore in between the
+# phrases in addition to single words. the delimiter in between the
 # concatenated words will be removed when the master list is created
 # -----------------------------------------------------------------------------
 
@@ -16,7 +16,6 @@ from __future__ import division
 import nltk
 import re
 import json
-import timeit
 
 def make_json_obj(filename):
     f = file(filename)
@@ -51,26 +50,33 @@ def fdist_phrases(corpus, phrase_length, occurrences, delimiter,
     for x in range(len(common_occurrences)):
         if common_occurrences[x][1] <= occurrences:
             break
-        master_list.append((common_occurrences[x][0].replace(delimiter, ' '),
-                           common_occurrences[x][1]))
+        common_occurrence_append = common_occurrences[x][0].\
+            replace(delimiter, ' ')
+        common_occurrence_append = common_occurrence_append.\
+            replace(' ,', ',')
+        master_list.append((common_occurrence_append, common_occurrences[x][1]))
     master_list = sorted(master_list, key=lambda byColumn: byColumn[0])
     master_list_letters_only = []
     for x in range(len(master_list)):
-        if re.search('[a-zA-Z]', master_list[x][0][0]):
+        if re.search('[a-zA-Z]', master_list[x][0][0]) and\
+                not re.search('\.', master_list[x][0]) and\
+                not re.search('!', master_list[x][0]):
             master_list_letters_only.append(master_list[x])
     return master_list_letters_only
 
 def get_autosuggestions(string, num_matches, search_set):
-    autosuggestions = []
     matches = [x for x in search_set if re.search(string, x[0][0:len(string)])]
     matches = sorted(matches, key=lambda byColumn: byColumn[1], reverse=True)
     L = len(matches)
     matches = matches[0:num_matches]
+    autosuggestions = []
     for x in range(L):
         if x == num_matches:
             break
         autosuggestions.append(matches[x][0])
     return autosuggestions
+
+# correct puncuation in list. can remove anything with a sentence transition
 
 conversations_text = make_json_obj('sample_conversations.json')
 csrep_message_list = grab_csrep_messages(conversations_text)
@@ -78,7 +84,7 @@ corpus = nltk.word_tokenize(csrep_message_list)
 autosuggestion_list = []
 for x in range(5):
     autosuggestion_list = fdist_phrases(corpus, x+1, 3, '_', autosuggestion_list)
-autosuggestion_list = set(autosuggestion_list)
+autosuggestion_set = set(autosuggestion_list)
 
-if __name__ == '__main__':
-    main()
+# if __name__ == '__main__':
+#    main()
